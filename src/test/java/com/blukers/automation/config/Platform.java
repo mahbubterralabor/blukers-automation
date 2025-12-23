0d8def0ce017a6165a8a840ef5a152b6b67f8edb
Mahbub Hossain <mahbub@terralabor.com>
2025-12-23 16:30:43 -0600

    Updated LocatorsLoad class

--- src/test/java/com/blukers/automation/config/Platform.java
+++ src/test/java/com/blukers/automation/config/Platform.java
@@ -1,6 +1,23 @@
 package com.blukers.automation.config;
 
 public enum Platform {
-    ANDROID,
-    IOS
+    // These constants are now "Self-Describing"
+    ANDROID("Android", "UiAutomator2"),
+    IOS("iOS", "XCUITest");
+
+    private final String platformName;
+    private final String automationName;
+
+    Platform(String platformName, String automationName) {
+        this.platformName = platformName;
+        this.automationName = automationName;
+    }
+
+    public String getPlatformName() {
+        return platformName;
+    }
+
+    public String getAutomationName() {
+        return automationName;
+    }
 }
--- src/test/java/com/blukers/automation/pages/android/HomePageAndroid.java
+++ src/test/java/com/blukers/automation/pages/android/HomePageAndroid.java
@@ -1,10 +1,17 @@
 package com.blukers.automation.pages.android;
 
+import com.blukers.automation.config.Platform;
 import com.blukers.automation.pages.base.BasePage;
 
 public class HomePageAndroid extends BasePage {
+    private static final String PAGE_NAME = "HomePage";
 
     public HomePageAndroid() {
-        super();
+
+        super(PAGE_NAME, Platform.ANDROID.getPlatformName());
+    }
+
+    public void tapLogin(){
+        click("loginButton");
     }
 }
